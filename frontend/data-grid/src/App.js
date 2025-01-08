2538e6549867434174ee520c2778992bd56bb039
Siddharth Srivastava <ssrivastava@ideatarmac.com>
2025-01-08 19:39:53 +0530

    Initial commit

---
+++ frontend/data-grid/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ProductDataGrid from './components/ProductDataGrid';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductDataGrid />
+    </div>
+  );
+}
+
+export default App;
